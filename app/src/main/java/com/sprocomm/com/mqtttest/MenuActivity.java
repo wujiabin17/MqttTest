6e050642414f7cfd7b90c6ebdf879569b9f667a1
sunzhongfang <zhongfang.sun@sprocomm.com>
2017-04-05 18:38:08 +0800

    修改翻页该标题.

--- app/src/main/java/com/sprocomm/com/mqtttest/MenuActivity.java
+++ app/src/main/java/com/sprocomm/com/mqtttest/MenuActivity.java
@@ -2,16 +2,21 @@ package com.sprocomm.com.mqtttest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
+import com.sprocomm.com.mqtttest.utils.ToastUtil;
+import com.sprocomm.com.mqtttest.widget.NormalTopBar;
 import com.sprocomm.com.mqtttest.widget.ParallaxScollListView;
 
-public class MenuActivity extends Activity {
+public class MenuActivity extends Activity implements View.OnClickListener, AbsListView.OnScrollListener {
     private ParallaxScollListView mListView;
     private ImageView mImageView;
+    private NormalTopBar mTopBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +31,13 @@ public class MenuActivity extends Activity {
         mListView = (ParallaxScollListView) findViewById(R.id.layout_listview);
         View header = LayoutInflater.from(this).inflate(R.layout.listview_header, null);
         mImageView = (ImageView) header.findViewById(R.id.layout_header_image);
-
+        mTopBar = (NormalTopBar) findViewById(R.id.menu_title);
+        mTopBar.setOnBackListener(this);
+        mTopBar.setTitle("好奇单车");
         mListView.setZoomRatio(ParallaxScollListView.ZOOM_X2);
         mListView.setParallaxImageView(mImageView);
         mListView.addHeaderView(header);
+        mListView.setOnScrollListener(this);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_expandable_list_item_1,
@@ -47,5 +55,26 @@ public class MenuActivity extends Activity {
                 }
         );
         mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mTopBar.getBackView()){
+            finish();
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int i) {
+        Log.d("simon","absListView:" + absListView + " i:" + i);
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+        if(i >= 1){
+            mTopBar.setTitle("个人中心");
+        }else{
+            mTopBar.setTitle("好奇单车");
+        }
     }
 }
